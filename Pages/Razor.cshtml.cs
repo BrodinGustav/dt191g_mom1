6ef34c20e557125123868d2c0b8a0dca93a38c5d
Gustav Brodin <brodin.gustav@gmail.com>
2025-01-23 22:13:57 +0100

    Commit - Lagt till funktion för utskrift om fredag

--- Pages/Razor.cshtml.cs
+++ Pages/Razor.cshtml.cs
@@ -1,4 +1,5 @@
 
+using System.Security.Cryptography.X509Certificates;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -6,33 +7,37 @@ namespace dt191g_mom1.Pages
 {
     public class RazorModel : PageModel
     {
-         public string CurrentDateTime { get; private set; }
+        public string CurrentDateTime { get; private set; }
 
-       
-    
-     //   public string DayOfTheWeek { get; set; }
+
+        //   public string DayOfTheWeek { get; set; }
         public void OnGet()
         {
 
-                // Sätt datum och tid som en egenskap
-        CurrentDateTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
-    
+            // Sätt datum och tid som en egenskap
+            CurrentDateTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
 
-            //Hur göra kollen ang fredag?
-            /*
-            DayOfTheWeek = "friday";
-            if (DateTime.Now == "friday")
+        }
+
+            //Fredags-koll
+            public string CheckIfFriday()
+        {
+
+            
+            if (DateTime.Now.DayOfWeek == DayOfWeek.Friday)
             {
-                DayOfTheWeek = "friday";
+                return "Det är fredag !";
             }
             else
             {
-j
+                return "Det är inte fredag";
             }
-    */
-    
+
         }
-    
+
+            //Loop över kurser
+
+
     }
 }
 
--- obj/Debug/net9.0/dt191g_mom1.AssemblyInfo.cs
+++ obj/Debug/net9.0/dt191g_mom1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dt191g_mom1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be5ae60c10c657a0b31711e42e97b9ce481dc9d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5df4f840259308ec591431bc33e0c51e845747c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("dt191g_mom1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dt191g_mom1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

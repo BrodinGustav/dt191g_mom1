66627d10d98a2c508fd88355974d0d990abc303d
Gustav Brodin <brodin.gustav@gmail.com>
2025-01-26 21:51:45 +0100

    Commit
    - Gjort headern responsiv för mindre skärmar genom col-md-6

--- obj/Debug/net9.0/dt191g_mom1.AssemblyInfo.cs
+++ obj/Debug/net9.0/dt191g_mom1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dt191g_mom1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ef34c20e557125123868d2c0b8a0dca93a38c5d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8196a4a5ce174df1992dd188587e0cff7b45b315")]
 [assembly: System.Reflection.AssemblyProductAttribute("dt191g_mom1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dt191g_mom1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

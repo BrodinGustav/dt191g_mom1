ac13b16cbbd0edff93675edb740234a484e052d4
Gustav Brodin <brodin.gustav@gmail.com>
2025-01-28 07:57:07 +0100

    Commit
    - Raderat BootStrap och justerat med egen CSS

--- obj/Debug/net9.0/dt191g_mom1.AssemblyInfo.cs
+++ obj/Debug/net9.0/dt191g_mom1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dt191g_mom1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66627d10d98a2c508fd88355974d0d990abc303d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+831f225e321aa03078b0dc7a44b77093d97356fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("dt191g_mom1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dt191g_mom1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
